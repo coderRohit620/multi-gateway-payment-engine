16cfee287313c9b8ae9f6c3d58d820addc846afb
coderRohit620 <rohitravi620@gmail.com>
2026-04-13 23:54:17 +0530

    feat: implement core application structure with Express, MongoDB connection, and error handling utilities

---
+++ src/server.js
@@ -0,0 +1,37 @@
+import dotenv from "dotenv"
+import connectDB from "./config/db"
+import { app } from "./app"
+
+dotenv.config({path: "./.env"})
+
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 5000, ()=>{
+            console.log(`Server is running at port :${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("MoNGO db connection failed !!!", err);
+        process.exit(1);
+    });
+
+
+/*
+import express from "express"
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/{DB_NAME}`)
+        app.on("error", (error) =>{
+            console.log("ERROR",error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () =>{
+            console.log(`App is Listening on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("ERROR",error)
+        throw err
+    }
+})()
+*/
